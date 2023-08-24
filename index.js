8d7a3b1e239ad04e1de22795104a935a5d125404
Jimmy Bergman <jimmy@sigint.se>
2023-08-24 14:52:37 +0200

    🔥 Final fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
 
 export const getExchangeRate = async (req, res) => {
-  const date = req.date || 'latest'
-  const from = (req.from || 'eur').toLowerCase()
-  const to = (req.to || 'sek').toLowerCase()
+  const date = req.query.date || 'latest'
+  const from = (req.query.from || 'eur').toLowerCase()
+  const to = (req.query.to || 'sek').toLowerCase()
   const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date}/currencies/${from}/${to}.json`)
   const data = await response.json()
   res.status(200).json(data)
