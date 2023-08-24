9942490fcbcaf5a3e2b182b7d598f6d1f72fa8f6
Jimmy Bergman <jimmy@sigint.se>
2023-08-24 14:48:50 +0200

    🐛 Fix for real

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export const getExchangeRate = async (req, res) => {
   const date = req.date || 'latest'
   const from = (req.from || 'eur').toLowerCase()
   const to = (req.to || 'sek').toLowerCase()
-  const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date}/currencies/${from}/${to}.json`).
+  const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date}/currencies/${from}/${to}.json`)
   const data = await response.json()
   res.status(200).json(data)
 }
